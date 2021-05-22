51720e6acb7f8a2da9ef28ace14cc398aea8805f
Denny K. Schuldt <dschuldt@outlook.com>
2021-05-22 13:42:07 -0500

    Updates

---
+++ precache-manifest.183df8689b213a5ee8e558d9bd3247f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5a11dc3c7d48591184a0c1b77e69ae",
+    "url": "/360/index.html"
+  },
+  {
+    "revision": "3fdedc793d8f407698d2",
+    "url": "/360/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "12b3552822fa2a0241b2",
+    "url": "/360/static/css/main.a19e36dd.chunk.css"
+  },
+  {
+    "revision": "3fdedc793d8f407698d2",
+    "url": "/360/static/js/2.2c9892bc.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/360/static/js/2.2c9892bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b3552822fa2a0241b2",
+    "url": "/360/static/js/main.5a351bce.chunk.js"
+  },
+  {
+    "revision": "1e9fb8d3700c5024ee08",
+    "url": "/360/static/js/runtime-main.687e5e44.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/360/precache-manifest.e587a0336da5b83642876a66f505b066.js"
+  "/360/precache-manifest.183df8689b213a5ee8e558d9bd3247f2.js"
 );
 
 self.addEventListener('message', (event) => {
